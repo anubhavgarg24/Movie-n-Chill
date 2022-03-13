a100df39d2d7df0227971bbfe15808e88fc561b2
Anubhav <anubhavgarg2001@gmail.com>
2022-03-13 14:33:31 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import SearchIcon from './search.svg'
 //API key = be667b1
 const API_URL = 'https://www.omdbapi.com?apikey=be667b1';
 
-const movie1 = {
-    "Title": "The Avengers",
-    "Year": "2012",
-    "imdbID": "tt0848228",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
-}
+// const movie1 = {
+//     "Title": "The Avengers",
+//     "Year": "2012",
+//     "imdbID": "tt0848228",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+// }
 
 const App = () => {
 
